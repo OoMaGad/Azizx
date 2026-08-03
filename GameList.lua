3ee52a7c4397903413a8462cd0308ae1de64fb58
ZanN Hub <kazutotababi1@gmail.com>
2026-08-03 15:52:20 -0500

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,3 +1,3 @@
 return {
-    [106920577206536] = "https://raw.githubusercontent.com/OoMaGad/A-Quiet-Place-Kazu-Hub/main/Script.lua.AquietPlace",
+    [106920577206536] = "https://raw.githubusercontent.com/OoMaGad/Azizx/refs/heads/main/Script.lua.AquietPlace",
 }
