626d4a43622760d386ecc128a0194d5f5a83bc8a
ZanN Hub <kazutotababi1@gmail.com>
2026-08-03 15:36:52 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,16 @@
+--!strict
 
+local HttpGet = game.HttpGet
+local GameId: number = game.GameId
+
+local Games: {[number]: string} = loadstring(
+    HttpGet(game, "https://raw.githubusercontent.com/OoMaGad/Azizx/refs/heads/main/GameList.lua")
+)() :: any
+
+local URL: string? = Games[GameId]
+if not URL then
+    warn("ZanN Hub: Unsupported game!")
+    return
+end
+
+loadstring(HttpGet(game, URL))()
